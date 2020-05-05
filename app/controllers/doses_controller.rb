b6dd9e808c929b7ae0a72188564888cf5fd39cc4
Yasir Malik <yasirrafiq786@gmail.com>
2020-05-05 19:35:41 +1000

    got rid of the error

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,11 +8,7 @@ class DosesController < ApplicationController
         @dose = Dose.new(dose_params)
         @cocktail = Cocktail.find(params[:cocktail_id])
         @dose.cocktail = @cocktail
-        if params[:dose][:ingredient_id] != ""
-            @ingredient = Ingredient.find(params[:dose][:ingredient_id])
-            @dose.ingredient = @ingredient
-        end
-               
+                   
         if  @dose.save
             redirect_to cocktail_path(@cocktail)
         else
@@ -39,7 +35,7 @@ class DosesController < ApplicationController
     private
 
     def dose_params
-        params.require(:dose).permit(:description)
+        params.require(:dose).permit(:description, :ingredient_id)
     end
 
 end
