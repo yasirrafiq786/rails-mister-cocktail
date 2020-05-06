fdcb7eb12984d5c1116cf149811281cb99715e18
Yasir Malik <yasirrafiq786@gmail.com>
2020-05-06 21:05:40 +1000

    Removed picture from index page and redesigned to include ratings and reviews pictures (not actual ones)

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -26,6 +26,6 @@ class CocktailsController < ApplicationController
     private
 
     def cocktails_params
-        params.require(:cocktail).permit(:name)
+        params.require(:cocktail).permit(:name, :photo)
     end
 end
